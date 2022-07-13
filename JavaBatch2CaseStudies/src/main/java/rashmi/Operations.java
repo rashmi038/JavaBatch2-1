576bca52127f84ef216ce690312606736bc297ea
rashmi038 <rashmi.tr@techtreeit.com>
2022-07-13 17:09:28 +0530

    Add files via upload

---
+++ JavaBatch2CaseStudies/src/main/java/rashmi/Operations.java
@@ -0,0 +1,31 @@
+package rashmi;
+
+public interface Operations {
+	static double add(double a, double b) {
+		return a + b;
+	}
+
+	static double sub(double a, double b) {
+		return a - b;
+	}
+
+	static double mul(double a, double b) {
+		return a * b;
+	}
+
+	static double div(double a, double b) {
+		return a / b;
+	}
+
+	static double percentage(double a, double b) {
+		return a * 100 / b;
+	}
+
+	static double mod(double a, double b) {
+		return a % b;
+	}
+
+	
+	}
+
+
